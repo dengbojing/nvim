06aa8fbc8c368cd39d636770aedd7054e721945b
dengbojing <dengbojing@outlook.com>
2024-03-13 13:43:14 +0800

    feat: configure for linux
    configure for linux

--- lua/user/globals.lua
+++ lua/user/globals.lua
@@ -1,7 +1,6 @@
 local g = vim.g
 vim.cmd[[colorscheme nord]]
-vim.opt.shell = 'pwsh'
-vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
+vim.opt.shell = 'bash'
 vim.opt.shellxquote = ''
 vim.api.nvim_set_option('clipboard','unnamed')
 vim.api.nvim_command('set fillchars=eob:`')
